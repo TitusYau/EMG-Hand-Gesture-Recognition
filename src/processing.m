38642a3a4fda711466650124bd220685e82f5cdc
Titus Yau <tcyau2@illinois.edu>
2025-08-31 23:01:44 -0500

    Removed dc offset + Performed bandpass/notch filtering + added graphing for testing

--- src/processing.m
+++ src/processing.m
@@ -27,8 +27,57 @@ for subject = subjects % For each subject
 
         fprintf("Loaded data for Subject %d\n", subject);
         fprintf('Data shape: %d samples x %d channels\n', size(emg));
-        fprintf('Number of movements: %d\n', max(restimulus));
+        fprintf('Number of movements: %d\n\n', max(restimulus));
     else
         warning('File not found: %s', filepath);
+    end
+end
+
+% Signal Preprocessing
+
+processed_data = {}
+
+for subject = subjects
+    if ~isempty(all_emg_data)
+        emg_raw = all_emg_data{subject};
+
+        % Remove the constant voltage offset caused by the interactions between the electrodes and skin
+        dc_offset = mean(emg_raw); 
+        emg_corrected = emg_raw - dc_offset;
+
+        % Use fifth-order Butterworth filter to perform bandpass filtering for the EMG signal
+        filter_order = 5;
+        normalized_freq = [15,95]/(200/2); % Frequencies normalized by Nyquist frequency
+        [b,a] = butter(filter_order, normalized_freq, 'bandpass'); 
+        emg_bandpassed = filtfilt(b,a, emg_corrected); % Filter
+
+        % Notch filter (removing interfering signals such as background hum produced by electrical grid)
+        notch_freq = 60;
+        Q = 30;
+        [b_notch, a_notch] = iirnotch(notch_freq / (200/2), notch_freq / (200/2) / Q);
+        emg_notched = filtfilt(b_notch, a_notch, emg_bandpassed);
+
+
+        % Plotting for testing purposes
+        t = (0:length(emg_raw)-1) / 200;
+        figure;
+        subplot(3, 1, 1);
+        plot(t, emg_raw);
+        title('EMG Raw');
+        xlabel('Time');
+        ylabel('Amplitude');
+
+        subplot(3, 1, 2);
+        plot(t, emg_bandpassed);
+        title('Bandpassed EMG');
+        xlabel('Time');
+        ylabel('Amplitude');
+
+        subplot(3, 1, 3);
+        plot(t, emg_notched);
+        title('Notched EMG');
+        xlabel('Time (s)');
+        ylabel('Amplitude');
+        break;
     end
 end
